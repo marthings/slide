9f6e06bbc714aad36737cac93d951b6b681fb095
Martin Berggren <martin@mwi.se>
2018-04-14 22:23:32 +0200

    Stuff works now

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,27 +1,55 @@
 class PartsController < ApplicationController
   before_action :set_presentation
+  before_action :set_part, except: [:new, :create]
 
   def show
   end
 
+  def new
+    @part = Part.new
+  end
+
   def edit
-    @part = Part.find(params[:id])
+    
   end
 
   def create
-    @presentation.parts.create! parts_params
+    @presentation.parts.create(parts_params)
     redirect_to @presentation
   end
 
   def update
-    @part = Part.find(params[:id])
-    @part.update(parts_params)
-    redirect_to @presentation
+    if @part.update(parts_params)
+      flash[:notice] = "Updated succesfully!"
+      redirect_to @presentation
+    else
+      flash[:error] = "There was an error updating the part"
+      render :show
+    end
+    
+  end
+
+  def destroy
+    @part.destroy
+    title = @part.title
+
+    if @part.destroy
+      flash[:notice] = "#{title} was deleted succesfully"
+      redirect_to @presentation
+    else
+      flash[:error] = "There was an error deleting the part"
+      render :show
+    end
+
   end
 
   private
     def set_presentation
       @presentation = Presentation.find(params[:presentation_id])
+    end
+
+    def set_part
+      @part = Part.find(params[:id])
     end
 
     def parts_params
