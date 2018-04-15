5c1a3c3bb99bb6c2b5c7cbab1e846c0c547343aa
Martin Berggren <martin@mwi.se>
2018-04-15 11:04:40 +0200

    Destroy parts when presentation is deleted. Markups fixes

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,6 +2,6 @@ class Presentation < ApplicationRecord
   validates :title, presence: true
   validates :cover, presence: true
   belongs_to :user
-  has_many :parts
+  has_many :parts, dependent: :destroy
   has_one_attached :cover
 end
