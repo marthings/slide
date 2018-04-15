eb03fcda220fdda90c8f5e3a522407f823fc48d1
Martin Berggren <martin@mwi.se>
2018-04-15 11:20:41 +0200

    Kinda works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require activestorage
+//= require direct_upload
+
+console.log("Hello")
--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,7 +1,8 @@
 class Presentation < ApplicationRecord
   validates :title, presence: true
-  validates :cover, presence: true
   belongs_to :user
   has_many :parts, dependent: :destroy
   has_one_attached :cover
 end
+
+
